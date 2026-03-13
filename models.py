7c8ba41f8dd7da134e1d734ed42e4d806d5cd364
SpecialNoel <jianminglin2893@gmail.com>
2026-03-13 14:42:29 -0400

    Add empty models.py with class list

---
+++ models.py
@@ -0,0 +1,5 @@
+# Classes designed:
+# - Customer: each customer has a name, can view and add orders to their purchase history
+# - FoodItem: each food item has a name, a price, a category indicating whether it is a drink or food, and a popularity rating based on customer reviews
+# - Menu: each menu has a list of food items. The manager of the app can edit the list, and the customer can view and filter the list.
+# - Order: each order has a customer, a list of food items, a total price for all the food in this order, and can edit food items in the order.
